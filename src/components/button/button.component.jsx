92dddb151c85e7eb4d32594303edcfb3c996f14e
Neha Gupta <er.nehaa.gupta@gmail.com>
2020-03-22 18:01:22 -0400

    Added the feature to group  all similar cart items

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './button.styles.scss';
 
-const Button = ({ children,isGoogleSignIn, ...otherProps}) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in':''} custom-button`} 
+const Button = ({ children,isGoogleSignIn,inverted, ...otherProps}) => (
+  <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in':''} custom-button`} 
     {...otherProps}
   >
     {children}
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions'
+import Button from '../button/button.component';
 import './collection-item.styles.scss'
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+  const { name, price, imageUrl } = item;
+  return(
   <div className='collection-item'>
     <div
       className='image'
@@ -13,6 +18,17 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
       <span className='name'>{ name }</span>
       <span className='price'>{ price }</span>
     </div>
+    <Button onClick={() => addItem(item)} inverted>
+      Add to Cart
+    </Button>
   </div>
-);
-export default CollectionItem;
+  )
+  };
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
